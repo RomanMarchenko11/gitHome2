b7279e18af4599a946882e2f85ee4c771b7e9c49
Roman Marchenko <rmarchenko11@icloud.com>
2023-06-30 15:38:37 +0300

    New File Main.java Changed README.md Changed .gitignore

--- LessonTen.java
+++ LessonTen.java
@@ -18,5 +18,32 @@ public class LessonTen {
         ryslan.setName("");
         ryslan.setNumStudentTicket("R342");
         ryslan.setAverageMark(110);
+
+//        Створюємо групу студентів (10 чол.)
+        Student[] groupOfStudents = new Student[10];
+
+        groupOfStudents[0] = new Student();
+        groupOfStudents[0].setName("Августін");
+        groupOfStudents[0].setNumStudentTicket("350");
+        groupOfStudents[0].setAverageMark(55);
+
+        groupOfStudents[1] = valiko;
+
+        groupOfStudents[2] = new Student();
+        groupOfStudents[2].setName("Назар");
+        groupOfStudents[2].setNumStudentTicket("400");
+        groupOfStudents[2].setAverageMark(65);
+
+        groupOfStudents[3] = new Student();
+        groupOfStudents[3].setName("Микита");
+        groupOfStudents[3].setNumStudentTicket("402");
+        groupOfStudents[3].setAverageMark(70);
+
+        System.out.println("Група стундентів:");
+        for (int i = 0; i <4; i++) {
+            System.out.print("Студент: " + groupOfStudents[i].getName());
+            System.out.print(", № квитка: " + groupOfStudents[i].getNumStudentTicket());
+            System.out.print(", Середня річна оцінка: " + groupOfStudents[i].getAverageMark()+";\n");
+        }
     }
 }
