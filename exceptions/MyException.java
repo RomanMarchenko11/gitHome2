2d46f73fe0165206994128a31f2abdec357a5095
Roman Marchenko <rmarchenko11@icloud.com>
2023-08-18 14:07:37 +0300

    New File Main.java Changed README.md Changed .gitignore

---
+++ exceptions/MyException.java
@@ -0,0 +1,45 @@
+package exceptions;
+
+import java.util.Objects;
+
+public class MyException extends RuntimeException{
+private String message;
+
+    public MyException() {
+    }
+
+    public MyException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyException that = (MyException) o;
+        return Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyException{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
+
